9ed23f55604a731453a1700350957f8e9ceeb145
Alex Snyder <63461278+asnyder8@users.noreply.github.com>
2021-09-27 21:00:59 -0500

    fixed d3.js and added some functionality to the index to demonstrate d3 csv parser

--- baseInit/d3.js
+++ baseInit/d3.js
@@ -1,15 +1,36 @@
-//gives access to D3 module
-const d3 = require("d3");
+//var d3 = require("d3");
 
 //test last e on page to make sure everything loaded
 function init() {
-    var testE = document.querySelector("#header1").innerHTML;
+    var testE = document.querySelector("#header3").innerHTML;
     console.log("Page load detected - testing last element on page...");
     console.log("Element text detected: " + testE);
 };
+
+function makeRectangle() {
+    var svg = d3.select("body").append("svg");
+
+    svg.attr("width", 250);
+    svg.attr("height", 250);
+
+    var rect = svg.append("rect");
+
+    rect.attr("x", 150);
+    rect.attr("y", 150);
+    rect.attr("width", 20);
+    rect.attr("height", 20);
+}
+
+function parseCSV() {
+    d3.csv("Dummy.csv",).then(function(data) {
+        for (var i = 0; i < data.length; i++){
+            console.log(data[i]);
+        }
+    });
+}
 
 //wait for page load to trigger init test boot
 window.onload = init;
 
 //***D3 LOGIC***
-//you'll want to add this functionality into the init() function so those 3 divs can become th
+//you'll want to add this functionality into the init() function
