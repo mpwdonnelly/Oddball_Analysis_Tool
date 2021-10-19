89c561904b062325dc4273e01ae1a8275b3721e5
mmelland2 <mmelland2@student.cvtc.edu>
2021-10-19 12:48:48 -0500

    Created basic checkboxes with a limit

--- baseInit/js/styles.js
+++ baseInit/js/styles.js
@@ -1,8 +1,17 @@
 //jQuery for styling webpages
 $(document).ready(function () {
 
+    var limit = 2;
+
     $("#splash>h1").click(function () {
         location.href = "welcome.html";
+    });
+
+    $("input.checkOption").on('change', function(evt) {
+        if($(this).siblings(":checked").length >= limit) {
+            this.checked = false;
+        }
+
     });
 
 
