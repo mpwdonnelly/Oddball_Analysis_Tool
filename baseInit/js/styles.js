c3c397e2a423078dd814fc4042de31ab795df093
mmelland2 <mmelland2@student.cvtc.edu>
2021-10-19 15:43:55 -0500

    Added comments to know what it does

--- baseInit/js/styles.js
+++ baseInit/js/styles.js
@@ -3,10 +3,12 @@ $(document).ready(function () {
 
     var limit = 2;
 
+    //When clicked moves pages
     $("#splash>h1").click(function () {
         location.href = "welcome.html";
     });
 
+    //Limits the amount of checked boxes in oldHTML
     $("input.checkOption").on('change', function(evt) {
         if($(this).siblings(":checked").length >= limit) {
             this.checked = false;
