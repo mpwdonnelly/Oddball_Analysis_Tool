92d4c836dc9e63890542f728912e61ae2517fb1d
mmelland2 <mmelland2@student.cvtc.edu>
2021-10-20 08:05:33 -0500

    Display correct text when button clicked

--- baseInit/js/styles.js
+++ baseInit/js/styles.js
@@ -13,7 +13,22 @@ $(document).ready(function () {
         if($(this).siblings(":checked").length >= limit) {
             this.checked = false;
         }
+    });
 
+    $("#showRaw").on("click", function() {
+        $("#rawGraph p").css("display", "block");
+    });
+
+    $("#showAverage").on("click", function() {
+        $("#averagedGraph p").css("display", "block");
+    });
+
+    $("#showBaseline").on("click", function() {
+        $("#baselineGraph p").css("display", "block");
+    });
+
+    $("#showMovingAverage").on("click", function() {
+        $("#movingAverageGraph p").css("display", "block");
     });
 
 
